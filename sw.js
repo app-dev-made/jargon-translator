905a6026da37dac54905315d4c6806d7b21c7668
app-dev-made <chougug@gmail.com>
2026-03-29 18:27:42 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,21 +1,26 @@
-const CACHE_NAME = 'jargon-lens-v5';
+const CACHE_NAME = 'jargon-lens-v8';
 const ASSETS = [
   './',
   './index.html',
   './manifest.json',
+  './icons/icon-72x72.png',
   './icons/icon-192x192.png',
   './icons/icon-512x512.png',
-  './icons/icon-72x72.png',
   './icons/apple-touch-icon.png'
 ];
 
+// Install Event: Caches all essential files
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('[SW] Caching app assets');
+      return cache.addAll(ASSETS);
+    })
   );
   self.skipWaiting();
 });
 
+// Activate Event: Deletes old caches to keep the app updated
 self.addEventListener('activate', (event) => {
   event.waitUntil(
     caches.keys().then((keys) => {
@@ -27,10 +32,11 @@ self.addEventListener('activate', (event) => {
   self.clients.claim();
 });
 
+// Fetch Event: Allows the app to work offline
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then((cached) => {
-      return cached || fetch(event.request);
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request);
     })
   );
 });
